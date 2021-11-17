641667ff561cf5eeba3c1cd7c446521e795e8a95
makar <misha.torno@gmail.com>
2021-11-18 00:57:37 +0500

    решил первый этап))))

--- filter.py
+++ filter.py
@@ -1,20 +1,21 @@
 from PIL import Image
 import numpy as np
+
 img = Image.open("img2.jpg")
 arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
+rowsCount = len(arr)
+colsCount = len(arr[1])
 i = 0
-while i < a - 11:
+while i < rowsCount:
     j = 0
-    while j < a1 - 11:
+    while j < colsCount:
         s = 0
         for n in range(i, i + 10):
             for n1 in range(j, j + 10):
-                n1 = arr[n][n1][0]
-                n2 = arr[n][n1][1]
-                n3 = arr[n][n1][2]
-                M = n1 + n2 + n3
+                r = arr[n][n1][0]
+                g = arr[n][n1][1]
+                b = arr[n][n1][2]
+                M = r // 3 + g // 3 + b // 3
                 s += M
         s = int(s // 100)
         for n in range(i, i + 10):
