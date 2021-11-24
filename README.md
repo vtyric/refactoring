77d9209c39bf3197225bcccbb7cd229f24e8bda4
vtyric <misha.torno@gmail.com>
2021-11-25 02:52:52 +0500

    добавил документацию и док тесты. исправил все ошибки по PEP8. удалил старое README

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
-# README refactroing Батуев Макар АТ-28
+# README refactoring Батуев Макар АТ-28
 
 ## Время работы:
 
-### Время работы filter.py (26433 ms)
+### Время работы filter.py (26433 ms):
 
 ![filter.py](refactoring-tasks-images/new-filter.PNG)
 
-### Время работы old-filter.py.py (1170 ms)
+### Время работы old-filter.py.py (1170 ms):
 
 ![old-filter.py](refactoring-tasks-images/old-filter.PNG)
 
-### Время работы filter_with_filename.py (81 ms)
+### Время работы filter_with_filename.py (81 ms):
 
 ![filter_with_filename.py](refactoring-tasks-images/filter_with_filename.PNG)
 
-### Объяснение результатов
+### Объяснение результатов:
 
 Самое большое время исполнения у первого фильтра, так там мы вводим данные с консоли. old-filter.py выполняет свою
 задачу гораздо медленнее filter_with_filename.py. В исправленном фильтре я избавился от ручных циклов, переделал их в
 матричные преобразования, что позволило повысить скорость выполнения кода.
+
+## Тесты и документация:
+
+### Пример теста:
+
+![average-test](refactoring-tasks-images/average-test.PNG)
+
+### Пример запуска правильного теста:
+
+![completed-test](refactoring-tasks-images/complete-test.PNG)
+
+### Пример неправильного теста:
+
+![failed-test](refactoring-tasks-images/failed-test.PNG)
--- filter.py
+++ filter.py
@@ -1,21 +1,57 @@
 from PIL import Image
 import numpy as np
+import doctest
 
 
-def read(filename):
+def read(filename: str):
+    """
+
+    :param filename: название файла
+    :return: np массив от фотографии с именем filename
+    """
     img = Image.open(filename)
     return np.array(img)
 
 
-def get_average(array, start_x, start_y, delta):
+def get_average(array: np.array, start_x: int, start_y: int, delta: int):
+    """
+
+    :param array: np массив
+    :param start_x: координата x
+    :param start_y: координата y
+    :param delta: смещение по x и y
+    :return: среднее значение в массиве в диапазоне start_x<=x<start_x+delta, start_y<=y<start_y+delta
+
+    >>> get_average(np.array([[1, 1, 1], [1, 1, 1]]), 0, 0, 1)
+    1.0
+    >>> get_average(np.array([[1, 1, 1], [1, 1, 1]]), 0, 0, 3)
+    1.0
+    >>> get_average(np.array([[1, 2, 3], [1, 2, 3]]), 0, 0, 3)
+    1.0
+    """
     return np.mean(array[start_x:start_x + delta, start_y:start_y + delta])
 
 
-def fill_array(array, start_x, start_y, delta, value):
+def fill_array(array: np.array, start_x: int, start_y: int, delta: int, value: float):
+    """
+    :param array: np массив
+    :param start_x: координата x
+    :param start_y: координата y
+    :param delta: смещение по x и y
+    :param value: значение для array[x][y][0], array[x][y][1], array[x][y][2] диапазона start_x<=x<start_x+delta,
+    start_y<=y<start_y+delta
+
+    """
     array[start_x:start_x + delta, start_y:start_y + delta] = np.array(3 * [value])
 
 
-def make_image_gray(array, puzzle_size, grayscale):
+def make_image_gray(array: np.array, puzzle_size: int, grayscale: float):
+    """
+    :param array: np массив
+    :param puzzle_size: размер блока
+    :param grayscale: градация серого
+
+    """
     max_x = len(array)
     max_y = len(array[1])
 
@@ -25,23 +61,29 @@ def make_image_gray(array, puzzle_size, grayscale):
             fill_array(array, x, y, puzzle_size, int(average // grayscale) * grayscale)
 
 
-def write(array, filename):
+def write(array: np.array, filename: str):
+    """
+
+    :param array: np массив
+    :param filename: название файла, куда произойдет запись результата
+    """
     res = Image.fromarray(array)
     res.save(filename)
 
 
 if __name__ == "__main__":
+    doctest.testmod()
     try:
         print("введите название картинки, которую нужно изменить:")
         read_name = input()
-        print("введите название файла, куда созранять картинку:")
+        print("введите название файла, куда сохранять картинку:")
         write_name = input()
-        print("введите два числа через запятую в таком формате:\nразмер мозайки, градация серого")
+        print("введите два числа через запятую в таком формате:\nразмер мозаики, градация серого")
         a, b = map(int, input().split(','))
         arr = read(read_name)
         make_image_gray(arr, a, b)
         write(arr, write_name)
-        print(f"измененая картинка({read_name}) находится в {write_name}")
-    except:
+        print(f"измененная картинка({read_name}) находится в {write_name}")
+    except SyntaxError:
         print("нет картинки, которую нужно изменить, или нет такого файлика, куда нужно ее сохранять, или вы ввели "
               "числа не в том формате")
